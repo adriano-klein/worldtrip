85daf3eeb69145f8cc1827e99bce8ae841dde0b0
adriano25klein@gmail.com <you@example.com>
2022-03-30 09:12:35 -0300

    Link na logo do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import { Box, Flex, Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Link } from "@chakra-ui/react";
 
 export function Header(){
   return(
-    <Flex as='header' dir="column" w="100%" h='20' maxWidth={1440} align="center" margin="0 auto">
-      <Image src="../images/Logo.svg" alt="Logo World Trip" boxSize="160" margin="0 auto"/>
+    <Flex as='header' dir="column" w="100%" h='20' maxWidth={1440} align="center">
+      <Link href="/" align="center" margin="0 auto">
+        <Image src="../images/Logo.svg" alt="Logo World Trip" boxSize="160" margin="0 auto"/>
+      </Link>
     </Flex>
   )
 }
